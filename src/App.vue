800760f926f92028436df2b286f45da3decd84c7
min yeo <korail7d@gmail.com>
2023-04-05 00:34:04 +0900

    v3

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="app">
-    <div v-if="state.account.id">
+    
+    <!-- 로그인시 -->
+    <div v-if="state.account.idNum">
       안녕하세요? {{ state.account.name }}님!
       <button @click="logout()">로그아웃</button>
     </div>
+
+    <!-- 비로그인 -->
     <div v-else>
       <label for="loginId">
         <span>아이디</span>
@@ -27,24 +31,29 @@ import { reactive } from "vue";
 
 export default {
   setup() {
+
+    //초기로그인값들. state는 현재상태므로 중요하다.
     const state = reactive({
       account:{
-        id:null,
+        idNum:null,
         name: "",
       },
+      //로그인 정보를 보내주기위한 form
       form:{
         loginId:"",
         loginPw:""
       },
-      loggedIn: false,
+      loggedIn: false,  
     });
 
+    //로그인버튼시 던질거임
     const submit = () => {
+      //객체로 post에 싣어서 던지기위해 args바디를 만들어줌
       const args = {
-
         loginId: state.form.loginId,
         loginPw: state.form.loginPw
       }
+
       axios.post("/api/account", args).then((res)=>{
         alert("로그인성공");
         state.account = res.data
@@ -54,15 +63,18 @@ export default {
       });
     };
 
+    //
     const logout =() =>{
       axios.delete("/api/account").then(()=>{
         alert("로그아웃했습니다");
-        state.account.id = null;
+        state.account.idNum = null;
         state.account.name = "";
 
       })
     }
-
+    
+    //로그인하고있으면 계정정보 받아오기
+    // axios.get("localhost:3000/api/account")앞에 호스트는 cors프록시로 인해 사라짐.
     axios.get("/api/account").then((res) => {
       state.account = res.data;
       // state.loggedIn 
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+// proxy설정.. /api로 시작하면 우회해서 요청하는것(3000에서 요청하듯)
 module.exports = {
   devServer:{
     proxy: {
